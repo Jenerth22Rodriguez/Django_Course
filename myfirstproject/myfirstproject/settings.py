64cbf37210ccd165650fc1247613d8956ebcf2f8
Jenerth Rodriguez <Connerjamestill@gmail.com>
2024-03-19 22:11:15 -0400

    Adding a response request

--- myfirstproject/myfirstproject/settings.py
+++ myfirstproject/myfirstproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # adding home  app to showcase our work
+    "home",
 ]
 
 MIDDLEWARE = [
