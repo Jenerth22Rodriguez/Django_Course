37f039cc8e0521c65ecea4b1211fc2ac6ff59fbc
Jenerth Rodriguez <Connerjamestill@gmail.com>
2024-03-19 22:46:09 -0400

    elimante the dependecie

--- myfirstproject/myfirstproject/urls.py
+++ myfirstproject/myfirstproject/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from home import views
 
-urlpatterns = [path("admin/", admin.site.urls), path("home", views.home)]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("home.urls"))]
